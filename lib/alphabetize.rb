9755e67595320bf88a9062acf4224387bbb4a363
derickcastillo38 <9derick@gmail.com>
2018-07-08 14:19:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,11 +4,9 @@ def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 
   split = arr.sort_by do |string|
-    string.split("")
-  end
-
-  split.map do |word|
-    esperanto.index(word)
+    string.split("").map do |word|
+      esperanto.index(word)
+    end
   end
 
 end
